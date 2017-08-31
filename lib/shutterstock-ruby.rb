0d8411f0bdf272afb42075313f62817dd3ec78d1
William Bowling <will@biteable.com>
2017-08-31 16:25:32 +1000

    Extract Configuration class

--- lib/shutterstock-ruby.rb
+++ lib/shutterstock-ruby.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'rest_client'
+require 'shutterstock-ruby/configuration'
 require 'shutterstock-ruby/connections'
 require 'shutterstock-ruby/images'
 require 'shutterstock-ruby/videos'
@@ -17,18 +18,5 @@ module ShutterstockRuby
       @images = Images.new(configuration)
     end
 
-    # Main configuration class.
-    class Configuration
-      attr_accessor :access_token
-      attr_accessor :api_client
-      attr_accessor :api_secret
-
-      def initialize(args)
-        @access_token = args[:access_token]
-        @api_client = args[:api_client]
-        @api_secret = args[:api_secret]
-      end
-    end
   end
-
 end
